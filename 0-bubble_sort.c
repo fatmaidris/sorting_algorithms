fd4fa299a197cf4e0c11b4658a4e6634ab3b6b96
fatma <fatma18650@gmail.com>
2024-05-05 11:22:08 -0700

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,13 +18,13 @@ void bubble_sort(int *array, size_t size)
 	i = 0;
 	while (i < size)
 	{
-		for (k = 0; k < size -1; k++)
+		for (k = 0; k < size - 1; k++)
 		{
 			if (array[k] > array[k + 1])
 			{
 				temp = array[k];
-				array[k] = array[k +1];
-				array[k +1] = temp;
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
 				print_array(array, size);
 			}
 		}
