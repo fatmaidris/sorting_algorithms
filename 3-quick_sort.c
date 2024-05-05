3523c7b9b7d7aeeb6b10b558d1f72d4df5d90b6a
fatma <fatma18650@gmail.com>
2024-05-05 16:38:56 -0700

    done

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -32,9 +32,9 @@ size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
 {
 	int i, j, pivot = array[hi];
 
-	for (i = j; j < hi; j++)
+	for (i = j = lo; j < hi; j++)
 		if (array[j] < pivot)
-			swap(array, size, &array[j], &array[i++])
+			swap(array, size, &array[j], &array[i++]);
 	swap(array, size, &array[i], &array[hi]);
 
 	return (i);
@@ -53,10 +53,11 @@ void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
 {
 	if (lo < hi)
 	{
-		size_t p = lomuto_paratition(array, size, lo, hi);
+		size_t p = lomuto_partition(array, size, lo, hi);
 
 		quicksort(array, size, lo, p - 1);
 		quicksort(array, size, p + 1, hi);
+	}
 }
 
 /**
