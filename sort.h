b45a0b725bf3d8d0563febba946434bf5614730f
fatma <fatma18650@gmail.com>
2024-05-05 11:17:48 -0700

    done

--- sort.h
+++ sort.h
@@ -21,8 +21,8 @@ typedef struct listint_s
 
 
 /* print */
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
@@ -36,6 +36,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
